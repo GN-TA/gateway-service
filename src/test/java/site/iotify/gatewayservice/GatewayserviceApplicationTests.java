1ca3996e09538749d083cffaf59462f3c9d2d52c
codeTheStudent <hjun303@gnu.ac.kr>
2024-11-19 15:53:19 +0900

    init : project initialize

---
+++ src/test/java/site/iotify/gatewayservice/GatewayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package site.iotify.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
