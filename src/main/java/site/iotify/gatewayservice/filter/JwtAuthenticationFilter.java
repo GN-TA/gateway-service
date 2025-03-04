0aee3efc4d60a40664c6079a11d00f288b54ccf6
qaw <osshun21@gmail.com>
2025-03-04 20:57:10 +0900

    fix : 에러 로그 출력

--- src/main/java/site/iotify/gatewayservice/filter/JwtAuthenticationFilter.java
+++ src/main/java/site/iotify/gatewayservice/filter/JwtAuthenticationFilter.java
@@ -134,6 +134,7 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
         System.out.println(">>> s = " + s);
 
         byte[] decodedKey = Base64.getDecoder().decode(s);
+        System.out.println(">>> decodedKey = " + s);
 
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedKey);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA"); // RSA 또는 EC
